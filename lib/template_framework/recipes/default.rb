6bd592020e6ff1f652aa1a566260d8b43c4b1fa1
Mio Nilsson <mionilsson@gmail.com>
2011-06-05 20:28:38 +0200

    javascripts and images now in app/assets

--- lib/template_framework/recipes/default.rb
+++ lib/template_framework/recipes/default.rb
@@ -2,13 +2,13 @@
 remove_file "README"
 remove_file "public/index.html"
 remove_file "public/robots.txt"
-remove_file "public/images/rails.png"
+remove_file "app/assets/images/rails.png"
 
 # Create project files
 create_file 'README'
 create_file 'log/.gitkeep'
 create_file 'tmp/.gitkeep'
 
-get "http://html5shiv.googlecode.com/svn/trunk/html5.js", "public/javascripts/html5.js"
+get "http://html5shiv.googlecode.com/svn/trunk/html5.js", "app/assets/javascripts/html5.js"
 
 apply templater.recipe('git')
