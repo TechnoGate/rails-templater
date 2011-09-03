618d05943b97019ca2f6141e11e0344fbe830ff3
Mio Nilsson <mionilsson@gmail.com>
2011-09-03 22:43:01 +0200

    update remarkable version

--- lib/template_framework/recipes/remarkable.rb
+++ lib/template_framework/recipes/remarkable.rb
@@ -1,6 +1,6 @@
 if templater.orm.mongoid?
-  gem 'remarkable_activemodel', '>=4.0.0.alpha4', :group => :test
-  gem 'remarkable_mongoid', :group => :test
+  gem "remarkable_activemodel", ">=4.0.0.alpha4", :group => :test
+  gem "remarkable_mongoid", "~> 0.6.0", :group => :test
   require_remarkable = "\nrequire 'remarkable/active_model'\nrequire 'remarkable/mongoid'"
 elsif templater.orm.active_record?
   gem 'remarkable_activerecord', '>=4.0.0.alpha4', :group => :test
