8aada4e612787112929f6a9510e5977cac116bfb
Mio Nilsson <mionilsson@gmail.com>
2011-09-03 22:41:20 +0200

    update mongoid version

--- lib/template_framework/recipes/mongoid.rb
+++ lib/template_framework/recipes/mongoid.rb
@@ -1,7 +1,7 @@
 say("\nReplacing ActiveRecord with Mongoid\n", Thor::Shell::Color::YELLOW )
 
-gem 'mongoid', '2.0.0.rc.7'
-gem 'bson_ext', '~> 1.2'
+gem "mongoid", "~> 2.2.0"
+gem "bson_ext", "~> 1.3.1"
 
 inject_into_file 'config/environment.rb', :after => "require File.expand_path('../application', __FILE__)\n" do
   "require 'yaml'\n"
