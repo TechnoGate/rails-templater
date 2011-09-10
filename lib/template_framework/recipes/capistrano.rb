29f46f752eec7cc85121246048fa711b28ae860a
Wael Nasreddine <wael.nasreddine@gmail.com>
2011-09-10 20:13:23 +0200

    MISC: Own capistrano!.

--- lib/template_framework/recipes/capistrano.rb
+++ lib/template_framework/recipes/capistrano.rb
@@ -2,55 +2,12 @@ c = ask("\n[Deployment] Would you like to use Capitrano for deployments? [Y|n]: 
 unless c == "n" || c == "no"
   # Dependencies
   gem "capistrano", "~> 2.8.0", :group => :development
+  gem "capistrano-exts", "~>1.8", :group => :development
 
-  # Configurations
-  create_file "Capfile", templater.load_template("Capfile", "capistrano")
-
-  def_app = `basename $(pwd)`.chomp
-  application = ask("What's the name of the applicaiton [#{def_app}]?", Thor::Shell::Color::BLUE)
-  application = def_app if application.blank?
-  domain = ask("What domain [talkative.se]?", Thor::Shell::Color::BLUE)
-  domain = "talkative.se" if domain.blank?
-  def_repo = `cat .git/config | grep github | cut -d '=' -f2 | cut -c2-`.chomp
-  repo = ask("Which repository [#{def_repo}]?", Thor::Shell::Color::BLUE)
-  repo = def_repo if repo.blank?
-  deploy_subdir = ask("Deploy subdir []?", Thor::Shell::Color::BLUE)
-  deploy_to = ask("Deploy to (eg: /var/www/talkative.se)?", Thor::Shell::Color::BLUE)
-  deploy_as = ask("Deploy as user [admin]?", Thor::Shell::Color::BLUE)
-  deploy_as = "admin" if deploy_as.blank?
-
-  file "config/deploy.rb", %Q{set :application, "#{application}"
-set :domain, "#{domain}"
-set :repository, "#{repo}"
-set :deploy_subdir, "#{deploy_subdir}"
-set :use_sudo, false
-set :deploy_to, "#{deploy_to}"
-set :deploy_via, :remote_cache
-set :user, "#{deploy_as}"
-set :scm, :git
-
-role :web, domain
-role :app, domain
-role :db, domain, :primary => true
-
-namespace :deploy do
-  task :start, :roles => :app do
-    run "touch \#{current_release}/tmp/restart.txt"
-  end
-
-  task :stop, :roles => :app do
-    # Do nothing
-  end
-
-  desc "Restart application"
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "touch \#{current_release}/tmp/restart.txt"
-  end
-end}
-
-  if deploy_subdir.blank?
-    run("cat config/deploy.rb | grep -v 'deploy_subdir' > /tmp/deploy.rb")
-    remove_file("config/deploy.rb")
-    copy_file("/tmp/deploy.rb", "config/deploy.rb")
+  templater.post_bundler do
+    run "capify ."
+    remove_file 'config/deploy.rb'
+    create_file 'config/deploy.rb', templater.load_template('config/deploy.rb', 'capistrano')
+    run "cap multistage:setup"
   end
 end
---
+++ lib/template_framework/templates/capistrano/config/deploy.rb
@@ -0,0 +1,39 @@
+# Adding necessary paths
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+
+#####################
+## CHANGE AFTER ME ##
+#####################
+
+set :application,           "what-is-the-application-name"
+set :repository,            "git://github.com/user/what-is-the-application-name.git"
+set :scm,                   :git
+set :git_enable_submodules, false
+
+# Stages
+# Stages can be really anything you want
+set :stages, [:development, :staging, :production]
+set :default_stage, :development
+
+# Capistrano extensions
+# Valid extensions: :multistage, :git, :deploy, :mysql, :rails, :contao, :contents, :god, :unicorn, :servers
+set :capistrano_extensions, [:multistage, :git, :deploy, :mysql, :rails, :contents, :god, :servers]
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+##################
+## DEPENDENCIES ##
+##################
+
+after "deploy", "deploy:cleanup" # keeps only last 5 releases
+
+###########################
+## DO NOT TOUCH AFTER ME ##
+###########################
+
+# Require capistrano-exts
+require 'capistrano-exts'
+
+# rvm bootstrap
+require "rvm/capistrano"
